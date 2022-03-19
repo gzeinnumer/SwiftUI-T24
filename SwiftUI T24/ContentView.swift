21253a1f2e5e2e79fac43528ded2160ed09fde59
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-03-19 16:07:53 +0700

    commit

--- SwiftUI T24/ContentView.swift
+++ SwiftUI T24/ContentView.swift
@@ -8,9 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var show = false
+    @Namespace var namespace
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            if !show{
+                ScrollView {
+                    HStack {
+                        VStack{
+                            Text("Title").foregroundColor(.white)
+                                .matchedGeometryEffect(id: "title", in: namespace)
+                        }
+                        .frame(width: 100, height: 100)
+                        .background(Rectangle().matchedGeometryEffect(id: "shape",in: namespace).frame(width: 100, height: 100))
+                        Rectangle()
+                            .frame(width: 100, height: 100)
+                        Spacer()
+                    }
+                }
+            } else{
+                VStack{
+                    Text("Title")
+                        .foregroundColor(.blue)
+                    .matchedGeometryEffect(id: "title", in: namespace)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 400)
+                .background(Rectangle().matchedGeometryEffect(id: "shape", in: namespace))
+            }
+        }
+        .onTapGesture {
+            withAnimation(.spring(response: 0.5, dampingFraction: 0.7)){
+                show.toggle()
+            }
+        }
     }
 }
 
